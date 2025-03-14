90e09fbe7cad0ca33fbc131a90d2ac0efe2820b0
Hank Zhao <hankzhao32@gmail.com>
2025-03-14 12:36:55 -0700

    Fix add task page

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -2,21 +2,21 @@ function writeTasks(taskname, desc, taskdate) {
     //define a variable for the collection you want to create in Firestore to populate data
     var tasks = db.collection("tasks");
 
-    tasks.add({
+    return tasks.add({
         name: taskname,
         description: desc,
         date: taskdate
     });
 }
 
-document.getElementById('submit').addEventListener('click', function () {
+document.getElementById('submit').addEventListener('click', async function () {
     title = document.getElementById("tasktitle").value;
     desc = document.getElementById("taskdesc").value;
     date = document.getElementById("taskdate").value;
 
-    writeTasks(title, desc, date);
-
+    await writeTasks(title, desc, date)
     window.location.href = "main.html"
+
 });
 
 
